20ddc6fa0ade4642e97d9d55433c8f87d0dac62d
Andrew Reitz <aj.reitz@gmail.com>
2019-09-17 14:44:36 -0500

    Upated Headers and a tag css

--- src/main/kotlin/cash/andrew/feeshu/index.kt
+++ src/main/kotlin/cash/andrew/feeshu/index.kt
@@ -2,7 +2,7 @@ import org.w3c.dom.Window
 import org.w3c.dom.asList
 import kotlin.js.Promise
 
-private const val HTML_TEMPLATE = """<!DOCTYPE html><html lang="en"><head> <meta charset="UTF-8"> <meta name="viewport" content="width=device-width, initial-scale=1.0"> <meta http-equiv="X-UA-Compatible" content="ie=edge"> <title>feeshu</title> <style>html{line-height:1.15;-webkit-text-size-adjust:100%}body{margin:0}main{display:block}h1{font-size:2em;margin:.67em 0}hr{box-sizing:content-box;height:0;overflow:visible}pre{font-family:monospace,monospace;font-size:1em}a{background-color:transparent}abbr[title]{border-bottom:none;text-decoration:underline;text-decoration:underline dotted}b,strong{font-weight:bolder}code,kbd,samp{font-family:monospace,monospace;font-size:1em}small{font-size:80%}sub,sup{font-size:75%;line-height:0;position:relative;vertical-align:baseline}sub{bottom:-.25em}sup{top:-.5em}img{border-style:none}button,input,optgroup,select,textarea{font-family:inherit;font-size:100%;line-height:1.15;margin:0}button,input{overflow:visible}button,select{text-transform:none}button,[type="button"],[type="reset"],[type="submit"]{-webkit-appearance:button}button::-moz-focus-inner,[type="button"]::-moz-focus-inner,[type="reset"]::-moz-focus-inner,[type="submit"]::-moz-focus-inner{border-style:none;padding:0}button:-moz-focusring,[type="button"]:-moz-focusring,[type="reset"]:-moz-focusring,[type="submit"]:-moz-focusring{outline:1px dotted ButtonText}fieldset{padding:.35em .75em .625em}legend{box-sizing:border-box;color:inherit;display:table;max-width:100%;padding:0;white-space:normal}progress{vertical-align:baseline}textarea{overflow:auto}[type="checkbox"],[type="radio"]{box-sizing:border-box;padding:0}[type="number"]::-webkit-inner-spin-button,[type="number"]::-webkit-outer-spin-button{height:auto}[type="search"]{-webkit-appearance:textfield;outline-offset:-2px}[type="search"]::-webkit-search-decoration{-webkit-appearance:none}::-webkit-file-upload-button{-webkit-appearance:button;font:inherit}details{display:block}summary{display:list-item}template{display:none}[hidden]{display:none}body{color:#c7cae2;background-color:#272b2a}header{text-align:center}h2,ul{text-align:center}li{list-style-type:none;padding-top:8px;text-align:center}</style></head><body> <header> <h1>feeshu</h1> </header> ${"\${shows}"}</body></html>"""
+private const val HTML_TEMPLATE = """<!DOCTYPE html><html lang="en"><head> <meta charset="UTF-8"> <meta name="viewport" content="width=device-width, initial-scale=1.0"> <meta http-equiv="X-UA-Compatible" content="ie=edge"> <title>feeshu</title> <style>html{line-height:1.15;-webkit-text-size-adjust:100%}body{margin:0}main{display:block}h1{font-size:2em;margin:.67em 0}hr{box-sizing:content-box;height:0;overflow:visible}pre{font-family:monospace,monospace;font-size:1em}a{background-color:transparent}abbr[title]{border-bottom:none;text-decoration:underline;text-decoration:underline dotted}b,strong{font-weight:bolder}code,kbd,samp{font-family:monospace,monospace;font-size:1em}small{font-size:80%}sub,sup{font-size:75%;line-height:0;position:relative;vertical-align:baseline}sub{bottom:-.25em}sup{top:-.5em}img{border-style:none}button,input,optgroup,select,textarea{font-family:inherit;font-size:100%;line-height:1.15;margin:0}button,input{overflow:visible}button,select{text-transform:none}button,[type="button"],[type="reset"],[type="submit"]{-webkit-appearance:button}button::-moz-focus-inner,[type="button"]::-moz-focus-inner,[type="reset"]::-moz-focus-inner,[type="submit"]::-moz-focus-inner{border-style:none;padding:0}button:-moz-focusring,[type="button"]:-moz-focusring,[type="reset"]:-moz-focusring,[type="submit"]:-moz-focusring{outline:1px dotted ButtonText}fieldset{padding:.35em .75em .625em}legend{box-sizing:border-box;color:inherit;display:table;max-width:100%;padding:0;white-space:normal}progress{vertical-align:baseline}textarea{overflow:auto}[type="checkbox"],[type="radio"]{box-sizing:border-box;padding:0}[type="number"]::-webkit-inner-spin-button,[type="number"]::-webkit-outer-spin-button{height:auto}[type="search"]{-webkit-appearance:textfield;outline-offset:-2px}[type="search"]::-webkit-search-decoration{-webkit-appearance:none}::-webkit-file-upload-button{-webkit-appearance:button;font:inherit}details{display:block}summary{display:list-item}template{display:none}[hidden]{display:none}body{color:#c7cae2;background-color:#272b2a}header{text-align:center}h2,ul{text-align:center}li{list-style-type:none;padding-top:8px;text-align:center}a{color:azure;}</style></head><body> <header> <h1>feeshu</h1> </header> ${"\${shows}"}</body></html>"""
 
 @JsModule("jsdom")
 external class jsdom {
@@ -18,6 +18,9 @@ external fun fetch(url: String): Promise<org.w3c.fetch.Response>
 @JsName("feeshu")
 fun feeshu(req: Request, res: Response) {
     val sets = req.query.sets?.toString()?.toInt() ?: 10
+
+    res.set("Cache-Control", "max-age=600000") // ten minutes
+    res.set("Content-Type", "text/html; charset=UTF-8")
 
     fetch("https://api.phish.net/v3/setlists/recent?apikey=FFC6B556891FFF7A4198&limit=${sets}")
         .then { r -> r.json().asDynamic().unsafeCast<PhishJson>() }
@@ -70,7 +73,7 @@ external interface Request {
 
 external interface Response {
     fun send(any: Any)
-    fun append(any: Any)
+    fun set(key: String, value: String)
 }
 
 external interface SetListData {
